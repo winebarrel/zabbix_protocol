e71a4d51370d44299ae8e2f0de8e11915139f858
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-03-08 11:53:53 +0900

    Fix error message

--- lib/zabbix_protocol.rb
+++ lib/zabbix_protocol.rb
@@ -8,22 +8,22 @@ module ZabbixProtocol
   # http://www.zabbix.org/wiki/Docs/protocols/zabbix_agent/1.4
   ZABBIX_HEADER  = "ZBXD"
   ZABBIX_VERSION = "\x01"
-  DATA_LEN_BYTES = 8
+  PAYLOAD_LEN_BYTES = 8
 
-  MIN_RESPONSE_LEN = ZABBIX_HEADER.length + ZABBIX_VERSION.length + DATA_LEN_BYTES
+  MIN_DATA_LEN = ZABBIX_HEADER.length + ZABBIX_VERSION.length + PAYLOAD_LEN_BYTES
 
-  def self.dump(data)
-    if data.is_a?(Hash)
-      data = MultiJson.dump(data)
+  def self.dump(payload)
+    if payload.is_a?(Hash)
+      payload = MultiJson.dump(payload)
     else
-      data = data.to_s
+      payload = payload.to_s
     end
 
     [
       ZABBIX_HEADER,
       ZABBIX_VERSION,
-      [data.length].pack('Q'),
-      data
+      [payload.length].pack('Q'),
+      payload
     ].join
   end
 
@@ -32,32 +32,32 @@ module ZabbixProtocol
       raise TypeError, "wrong argument type #{data.class} (expected String)"
     end
 
-    if data.length < MIN_RESPONSE_LEN
+    if data.length < MIN_DATA_LEN
       raise Error, "data length is too short (data: #{data.inspect})"
     end
 
-    data = data.dup
-    header = data.slice!(0, ZABBIX_HEADER.length)
+    sliced = data.dup
+    header = sliced.slice!(0, ZABBIX_HEADER.length)
 
     if header != ZABBIX_HEADER
       raise Error, "invalid header: #{header.inspect} (data: #{data.inspect})"
     end
 
-    version = data.slice!(0, ZABBIX_VERSION.length)
+    version = sliced.slice!(0, ZABBIX_VERSION.length)
 
     if version != ZABBIX_VERSION
       raise Error, "unsupported version: #{version.inspect} (data: #{data.inspect})"
     end
 
-    data_len = data.slice!(0, DATA_LEN_BYTES)
-    data_len = data_len.unpack("Q").first
+    payload_len = sliced.slice!(0, PAYLOAD_LEN_BYTES)
+    payload_len = payload_len.unpack("Q").first
 
-    if data_len != data.length
-      raise Error, "invalid data length: expected=#{data_len}, actual=#{data.length} (data: #{data.inspect})"
+    if payload_len != sliced.length
+      raise Error, "invalid payload length: expected=#{payload_len}, actual=#{sliced.length} (data: #{data.inspect})"
     end
 
     begin
-      MultiJson.load(data)
+      MultiJson.load(sliced)
     rescue MultiJson::ParseError
       data
     end
